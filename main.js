39f98009a1769a211e482787859c1bc5d1391e13
netcy <a@nc32.top>
2023-04-15 11:38:29 +0800

    progressing(get request successfully)

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
-
+// app.use(express.json());
+app.use(bodyParser.json());
+// app.use(express.urlencoded({ extended: true }));
 app.post("/*", (req, res) => {
+    console.log(req.headers['content-type']); // log the content type header
+    console.log(req.headers['content-length']); // log the content length header
+    console.log(req.body); // log the plain-text request body
     console.log(req);
     console.log(req.body);
     res.end("Hello Login");
